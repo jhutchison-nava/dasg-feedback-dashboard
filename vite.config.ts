24bb33e1c894dc3a94121e2580324d8e48818208
Jonathan Hutchison <jonathanhutchison@navapbc.com>
2026-03-23 14:24:11 -0400

    Update repo name

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,7 @@ import { fileURLToPath, URL } from 'node:url'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/jhutchison-nava/dasg-feedback-dashboard/',
+  base: '/dasg-feedback-dashboard/',
   plugins: [
     tailwindcss(),
     tanstackRouter({
